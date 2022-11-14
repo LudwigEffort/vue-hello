2600597944202743907abc4e4dbdaea44c1542b6
LudwigEffort <dr.feynman65@gmail.com>
2022-11-14 09:50:54 +0100

    add img

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Word'
+    message: 'Hello Word',
+    image: 'https://picsum.photos/id/237/200/300'
   }
 })
